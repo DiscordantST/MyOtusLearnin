e9a05b2fd1b4630c67e2a7142bfa6a3bf1765f6e
DiscordantST <discordantstas@gmail.com>
2022-12-07 22:23:34 +0300

    "Refactoring main.py

--- homework_01/main.py
+++ homework_01/main.py
@@ -13,24 +13,43 @@ def power_numbers(*sequence):
     return [digit ** 2 for digit in sequence]
 
 
-"""
-Constants that determine which scenario the calculation will follow
-"""
+#Constants that determine which scenario the calculation will follow
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
 
-def is_prime(digit):
+def prime_number(digit_from_sequence):
     """
-    :param digit: PRIME digit
-    :return: True if digit is PRIME, False is digit is not PRIME
+    A prime number is an integer that is only divisible by 1 and by itself without a remainder
+    :param digit_from_sequence: sequence number
+    :return: True if digit is PRIME
     """
     count = 0
-    for elem in range(2, digit // 2 + 1):
-        if digit % elem == 0:
+    for elem in range(2, digit_from_sequence // 2 + 1):
+        if digit_from_sequence % elem == 0:
             count += 1
-    if count <= 0:
+    if count <= 0 and digit_from_sequence > 1:
+        return True
+
+
+def odd_number(digit_from_sequence):
+    """
+    An odd number is an integer that is not divisible by 2 without a remainder
+    :param digit_from_sequence: sequence number
+    :return: True if elem is ODD
+    """
+    if digit_from_sequence % 2 != 0:
+        return True
+
+
+def even_number(digit_from_sequence):
+    """
+    An even number is an integer that is divisible by 2 without a remainder
+    :param digit_from_sequence: sequence number
+    :return: True if elem is EVEN
+    """
+    if digit_from_sequence % 2 == 0:
         return True
 
 
@@ -38,17 +57,16 @@ def filter_numbers(sequence, filter_type):
     """
     A function that receives a sequence of natural numbers and, depending on the filter_type,
     gives a list of numbers (even, odd, or prime) as an output
-    An even number is an integer that is divisible by 2 without a remainder
-    An odd number is an integer that is not divisible by 2 without a remainder
-    A prime number is an integer that is only divisible by 1 and by itself without a remainder
-
     :param sequence: number sequence integer. example 1, 2, 3, 4, 5, 6, 7
     :param filter_type: ODD/EVEN/PRIME digit. example: filter_type == ODD or filter_type == EVEN or filter_type == PRIME
     :return: list of integer digits. (even, odd, or prime) example: [2, 4, 6]
     """
     if filter_type == ODD:
-        return [digit for digit in sequence if digit % 2 != 0]
+        odd_number_list = filter(odd_number, sequence)
+        return list(odd_number_list)
     if filter_type == EVEN:
-        return [digit for digit in sequence if digit % 2 == 0]
+        even_number_list = filter(even_number, sequence)
+        return list(even_number_list)
     if filter_type == PRIME:
-        return [digit for digit in sequence if is_prime(digit) is True and digit > 1]
+        prime_number_list = filter(prime_number, sequence)
+        return list(prime_number_list)
