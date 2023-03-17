325aa2b015b6c0426d6d8c6c2379ca2dadd4d732
DiscordantST <discordantstas@gmail.com>
2023-03-17 17:02:13 +0300

    refactoring title for htlm pages solution homework_08

--- homework_08/blog_project_otus_homework/blog/models.py
+++ homework_08/blog_project_otus_homework/blog/models.py
@@ -12,5 +12,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):  # Тут мы создали новый метод
+    def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.id)])
